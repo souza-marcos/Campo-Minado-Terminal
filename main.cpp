9a4958b1046cb2331e23749fe4e64c8f172fab02
Marcos Daniel <marcosnumer11@gmail.com>
2021-07-15 11:04:38 -0300

    Fixing Add bombs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+
 #include <windows.h>
 #include <string>
 //#include <conio.h>
@@ -7,7 +8,7 @@ using namespace std;
 
 #define VERSION_GAME 0.1
 
-int field[5][5], option = 0, level = 0;
+int field[5][5], option = 0, level = 0, fieldMirror[5][5];
 string playerName = "";
 string msgBeginner = "O Nivel facil tem 5 minas espalhadas pelo campo.\n";
 string msgEasy = "O Nivel facil tem 5 minas espalhadas pelo campo.\n";
@@ -86,15 +87,136 @@ void drawField()
         cout << "Linha " << i << " -> "; //1space
         for (int j = 0; j < 5; j++)
         {
-            cout << " " << ((field[i][j] != -1) ? to_string(field[i][j]) : ("*")) << " |";
+            /**
+             * TODO Change "field" per "fieldMirror"
+             */
+            //cout << " " << (field[i][j] == -2 ? " " : (field[i][j] == -1 ? "*" : to_string(field[i][j]))) << " |";
+            cout << " " << field[i][j] << " |";
         }
-        cout << endl
-             << "\t   ";
+        cout << "\n\t   ";
         for (int i = 0; i < 5; i++)
         {
             cout << "---+";
         }
         cout << endl;
+    }
+}
+
+/**
+ * Retorna true se 
+ */
+bool verifyBomb()
+{
+    return true;
+}
+
+void addBombs(int rowInitial, int colInitial)
+{
+    int seed = time(NULL);
+    srand(seed);
+    int bombs = (level / 8) * 25;
+    int rowSelected, colSelected;
+    for (int i = 0; i < bombs; i++)
+    {
+        while (true)
+        {
+            rowSelected = (rand() % 6) - 1;
+            colSelected = (rand() % 6) - 1;
+            if ((rowSelected != rowInitial || colSelected != colInitial) && field[rowSelected][colSelected] != -1)
+            {
+                field[rowSelected][colSelected] = -1;
+                break;
+            }
+        }
+    }
+}
+
+void mountCel(int *cel)
+{
+    if (*cel != -1)
+    {
+        *cel++;
+    }
+}
+
+void mountField()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (field[i][j] == -1)
+            {
+                if (i == 0)
+                {
+                    if (j == 0)
+                    {
+                        mountCel(&field[i][j + 1]);
+                        mountCel(&field[i + 1][j + 1]);
+                    }
+                    else if (j == 4)
+                    {
+                        mountCel(&field[i][j - 1]);
+                        mountCel(&field[i + 1][j - 1]);
+                    }
+                    else
+                    {
+                        mountCel(&field[i][j + 1]);
+                        mountCel(&field[i][j - 1]);
+                        mountCel(&field[i + 1][j - 1]);
+                        mountCel(&field[i + 1][j + 1]);
+                    }
+                    mountCel(&field[i + 1][j]);
+                }
+                else if (i == 4)
+                {
+                    if (j == 0)
+                    {
+                        mountCel(&field[i][j + 1]);
+                        mountCel(&field[i - 1][j + 1]);
+                    }
+                    else if (j == 4)
+                    {
+                        mountCel(&field[i][j - 1]);
+                        mountCel(&field[i - 1][j - 1]);
+                    }
+                    else
+                    {
+                        mountCel(&field[i][j + 1]);
+                        mountCel(&field[i][j - 1]);
+                        mountCel(&field[i - 1][j - 1]);
+                        mountCel(&field[i - 1][j + 1]);
+                    }
+                    mountCel(&field[i - 1][j]);
+                }
+                else
+                {
+                    if (j == 0)
+                    {
+                        mountCel(&field[i][j + 1]);
+                        mountCel(&field[i + 1][j + 1]);
+                        mountCel(&field[i - 1][j + 1]);
+                    }
+                    else if (j == 4)
+                    {
+                        mountCel(&field[i][j - 1]);
+                        mountCel(&field[i + 1][j - 1]);
+                        mountCel(&field[i - 1][j - 1]);
+                    }
+                    else
+                    {
+                        mountCel(&field[i - 1][j - 1]);
+                        mountCel(&field[i - 1][j + 1]);
+                        mountCel(&field[i][j + 1]);
+                        mountCel(&field[i + 1][j + 1]);
+                        mountCel(&field[i + 1][j - 1]);
+                        mountCel(&field[i][j - 1]);
+                    }
+                    mountCel(&field[i - 1][j]);
+                    mountCel(&field[i + 1][j]);
+                }
+            }
+        }
     }
 }
 
@@ -132,9 +254,24 @@ void newGame()
             break;
         }
         pause();
-    } while (level<1 | level> 4);
-    clear();
-    drawField();
+    } while (level < 1 || level > 4);
+    int turn = 1;
+    while (true)
+    {
+        clear();
+        drawField();
+        int row, col;
+        cout << "Digite a linha desejada: ";
+        cin >> row;
+        cout << "Digite a coluna desejada: ";
+        cin >> col;
+
+        if (turn == 1)
+        {
+            addBombs(row, col);
+        }
+        turn++;
+    }
 }
 
 int main()
@@ -144,15 +281,23 @@ int main()
      */
     for (int i = 0; i < 5; i++)
     {
-        for (int j = 0; j < 5; j++)
-        {
-            field[i][j] = 0;
+        for (int j = 0; j < 5; j++) // -2 -> vazio
+        {                           //
+            field[i][j] = 0;        //
+            fieldMirror[i][j] = -2;
         }
     }
 
+    //Test
+
+    level = 4;
+    addBombs(1, 1);
+    mountField();
     drawField();
-    pause();
+
     return 0;
+
+    //End Test
 
     do
     {
